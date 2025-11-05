5ffb1e9bc12a021fb5de5d43ff017d31d102cd19
Geveriell <erzaardhyaputra@gmail.com>
2025-11-05 08:36:13 +0700

    modifikasi percobaan 1.1

--- ArrayBilangan07.java
+++ ArrayBilangan07.java
@@ -1,17 +1,11 @@
 public class ArrayBilangan07 {
     public static void main(String[] args) {
-        int[] bil = new int[4]; // membuat banyak array
 
-        // elemen arrayy
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        // Inisialisasi langsung
+        int[] bil = {5, 13, -7, 17};
 
-        // menampilkan isi array
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
