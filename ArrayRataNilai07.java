dd1559623bf65c5e6fa26078152fbb816a0cc652
Geveriell <erzaardhyaputra@gmail.com>
2025-11-05 09:30:36 +0700

    modifikasi 3.1

--- ArrayRataNilai07.java
+++ ArrayRataNilai07.java
@@ -8,11 +8,17 @@ public class ArrayRataNilai07 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;   
+        int lulus = 0; // Tambahan: untuk menghitung mahasiswa dengan nilai > 70
 
         // Input nilai mahasiswa
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + " : ");
             nilaiMhs[i] = sc.nextInt();
+
+            // Tambahan: cek apakah mahasiswa lulus
+            if (nilaiMhs[i] > 70) {
+                lulus++;
+            }
         }
 
         // penjumlahan seluruh nilai mahasiswa
@@ -25,6 +31,9 @@ public class ArrayRataNilai07 {
 
         // Menampilkan hasil rata-rata
         System.out.println("Rata-rata nilai = " + rata2);
+
+        // Tambahan: menampilkan jumlah mahasiswa yang lulus (>70)
+        System.out.println("Jumlah mahasiswa yang lulus : " + lulus);
 
         sc.close();
     }
