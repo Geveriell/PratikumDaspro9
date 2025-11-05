340da0f168f2e87f8e60dfbf66a0a4be59e271ec
Geveriell <erzaardhyaputra@gmail.com>
2025-11-05 08:10:32 +0700

    percobaan 1

---
+++ ArrayBilangan07.java
@@ -0,0 +1,17 @@
+public class ArrayBilangan07 {
+    public static void main(String[] args) {
+        int[] bil = new int[4]; // membuat banyak array
+
+        // elemen arrayy
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+
+        // menampilkan isi array
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
