2b76532439036741649f3fac072ea1a5b33bbbc7
Geveriell <erzaardhyaputra@gmail.com>
2025-11-05 08:45:31 +0700

    percobaan 2

---
+++ ArrayNilai07.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+
+public class ArrayNilai07 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];// Membuat jumlah aray
+
+        // Input nilai akhir
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+
+        System.out.println(); 
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        }
+
+        sc.close();
+    }
+}
